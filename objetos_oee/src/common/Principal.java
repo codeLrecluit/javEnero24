52afae45127c87028f0cb6456d59a36e67ed21a2
Osvaldo44M <osvaldoee44@gmail.com>
2024-01-08 17:05:22 -0600

    se añade codigo de sobreescritura y composicion

--- objetos_oee/src/common/Principal.java
+++ objetos_oee/src/common/Principal.java
@@ -23,9 +23,21 @@ public class Principal {
 		SmartTv stv1 = new SmartTv();
 		
 		SmartTv stv2 = new SmartTv(55.6, "LG", 3578.99, "4K", "LED", "ROKU TV");
+
+		Camara cam1 = new Camara();
+		Camara cam2 = new Camara();
+		Camara cam3 = new Camara();
 		
+		Camara [] camaras = {cam1, cam2, cam3};
 		
+		Celular c1 = new Celular("s23 Ultra", "Samsung", 5000.00, 6.8, pa2, camaras);
+		System.out.println(c1);
 		
+		SistemaOperativo so = new SistemaOperativo("Android", "Android", "Version 11");
+		Smartphone sm1 = new Smartphone("NOTE 9", "SAMSUNG", 1200.00, 4.0, pa2, camaras, so);
+		
+		sm1.reproducirMusica();
+		sm1.hacerLlamadas("4567777882");
 	}
 	
 
